155bec7e0a0faaa24d0543b61418f8fae23e4b79
slinx <373942445@qq.com>
2018-12-11 11:53:23 +0800

    固废更新

---
+++ assets/js/com/index.js
@@ -0,0 +1,48 @@
+//js
+$("input[type='text']").keyup(function(){
+    var c=$(this);
+    if(/[^\d.]/.test(c.val())){
+        var temp_amount=c.val().replace(/[^\d.]/g,'');
+        $(this).val(temp_amount);
+    }
+});
+$(function () {
+    $('#reset').on('click', function () {
+        $('#numInput').val('');
+        console.log(countInput);
+        if ($('#countInput').val() === '18000') {
+            return
+        } else {
+            $('#countInput').val('');
+        }
+    })
+    $('#sum').on('click',function () {
+        if($('#numInput').val() == '' || $('#countInput').val() == ''){
+            $('.alert-tip').addClass('active');
+            setTimeout(function () {
+                $('.alert-tip').removeClass('active');
+            },2000)
+            return false
+        }
+        $('.result').toggleClass('active');
+        var numInput = $('#numInput').val();
+        var countInput = $('#countInput').val();
+        var outTimeResult = numInput * countInput * 0.388;
+        var firstTimeResult = numInput * countInput * 0.0026;
+        var saveCount = outTimeResult - firstTimeResult;
+        var saveTipCount = parseInt(saveCount/26);
+        $('.outTimeResult').html(outTimeResult.toFixed(2));
+        $('.firstTimeResult').html(firstTimeResult.toFixed(2));
+        $('.save__content').html(saveCount.toFixed(2));
+        $('.save__tip__count').html(saveTipCount.toFixed(2));
+        $('.save_pecent').html((parseFloat(saveCount/outTimeResult)*100).toFixed(1) +'%' || '0.0%');
+    })
+    $('.back').on('click',function(){
+        $('.result').toggleClass('active');
+    })
+})
+
+//style.less
+require('../../css/bootstrap.min.css');
+require('../../css/animate.css');
+require('../../less/style.less');
